2e4975e446d4875efc9a04bb7a0b15df0b547373
kimhakmelody <kimhakmelody@gmail.com>
2026-04-29 23:42:29 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Index from "./pages/index"; // ប្រើ i តូច បើឈ្មោះ file គឺ index.tsx
+import Index from "./pages/index.tsx"; // បន្ថែម .tsx នៅខាងចុងឱ្យច្បាស់
 
 const App = () => (
   <Routes>

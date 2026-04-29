39d93a016eb187e4648eea2cd6de755cc1a287c8
kimhakmelody <kimhakmelody@gmail.com>
2026-04-29 23:27:50 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/index";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
+import { Routes, Route } from "react-router-dom";
+import Index from "./pages/index"; // ប្រើ index (អក្សរតូច)
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <Routes>
+    <Route path="/" element={<Index />} />
+  </Routes>
 );
 
 export default App;

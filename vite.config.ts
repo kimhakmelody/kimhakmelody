fdfaa8ef0eb2ce4a15859c523bbb9f7ebfeb1065
kimhakmelody <kimhakmelody@gmail.com>
2026-04-29 22:31:18 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/kimhakmelody/", // បន្ថែមបន្ទាត់នេះ ដើម្បីឱ្យដើរលើ GitHub Pages
+  base: "/kimhakmelody/",
   server: {
     host: "0.0.0.0",
     port: 8080,

576958cbac4035d96d3b3ba53304281de992ccca
kimhakmelody <kimhakmelody@gmail.com>
2026-04-29 22:25:40 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/kimhakmelody/", // បន្ថែមបន្ទាត់នេះ ដើម្បីឱ្យដើរលើ GitHub Pages
   server: {
     host: "0.0.0.0",
     port: 8080,

98197e714af4ca6017dc9b41cb425ea65491bef8
kimhakmelody <kimhakmelody@gmail.com>
2026-04-30 07:52:53 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Index from "./pages/index"; 
 
-const App = () => (
-  <Routes>
-    <Route path="/" element={<Index />} />
-  </Routes>
-);
+const App = () => {
+  return (
+    <Routes>
+      {/* ប្រើ path="*" ដើម្បីឱ្យវាក្តោបយកគ្រប់ផ្លូវទាំងអស់ ការពារការរកផ្លូវមិនឃើញ */}
+      <Route path="/" element={<Index />} />
+      <Route path="*" element={<Index />} /> 
+    </Routes>
+  );
+};
 
 export default App;

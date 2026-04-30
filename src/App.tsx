19d1998546165a8723208a3eb117ad86092dc0e0
kimhakmelody <kimhakmelody@gmail.com>
2026-04-30 09:43:45 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/index"; // ឆែកអក្សរ i ឱ្យច្បាស់
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Navigation from "./components/Navigation";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import BackToTop from "./components/BackToTop";
 
-const App = () => {
+function App() {
   return (
-    <BrowserRouter basename="/kimhakmelody">
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="*" element={<Index />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider>
+      <div className="min-h-screen bg-background text-foreground transition-colors duration-300">
+        <Navigation />
+        <main>
+          <Hero />
+          <About />
+          <Skills />
+          <Projects />
+          <Contact />
+        </main>
+        <Footer />
+        <BackToTop />
+      </div>
+    </ThemeProvider>
   );
-};
+}
 
 export default App;

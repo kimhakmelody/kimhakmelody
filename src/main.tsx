12a68816081228a0ec514c04d3ba1579665844da
kimhakmelody <kimhakmelody@gmail.com>
2026-04-30 09:44:12 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './index.css' // ប្រាកដថាមាន File CSS នេះ
 
-createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
     <App />
-);
+  </React.StrictMode>,
+)

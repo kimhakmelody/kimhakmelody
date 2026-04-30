6a8ce61d1a0185bc77c2040319b37660b8387403
kimhakmelody <kimhakmelody@gmail.com>
2026-04-30 09:12:00 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { Routes, Route } from "react-router-dom";
-import Index from "./pages/index"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/index"; // ឆែកអក្សរ i ឱ្យច្បាស់
 
 const App = () => {
   return (
-    <Routes>
-      {/* ប្រើ path="*" ដើម្បីឱ្យវាក្តោបយកគ្រប់ផ្លូវទាំងអស់ ការពារការរកផ្លូវមិនឃើញ */}
-      <Route path="/" element={<Index />} />
-      <Route path="*" element={<Index />} /> 
-    </Routes>
+    <BrowserRouter basename="/kimhakmelody">
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="*" element={<Index />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
